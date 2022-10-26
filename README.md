7d6b53edc87eeeea7522a086a46e5d722cc18549
jvier95 <javiruchorosito@gmail.com>
2022-10-26 13:57:00 -0600

    changes and updeats, this is a flecth clone

--- README.md
+++
@@ -1,2 +0,0 @@
-# cse210-06
-Final game
---
+++ fetch/game/scripting/collide_dog_action.py
@@ -0,0 +1,14 @@
+from constants import *
+from game.casting.sound import Sound
+from game.scripting.action import Action
+
+
+class CollideDogAction(Action):
+
+    def __init__(self, physics_service, audio_service):
+        self._physics_service = physics_service
+        self._audio_service = audio_service
+        
+    def execute(self, cast, script, callback):
+        pass
+  
